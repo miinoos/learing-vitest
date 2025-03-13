671bceaeedbd65b8662e2286a8fe933c58e2e6c8
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2025-03-13 23:44:05 +0530

    First Test : Button With Role

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
+test("button starts with the correct color", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument(); //asserting
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
 });
+
+test("button starts with the correct color after click", () => {});
+
+test("button starts with the correct text after click", () => {});
