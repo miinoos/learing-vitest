53d7b7e7ab9190769b64cfb238baeecac0eb6d09
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2025-03-15 14:49:48 +0530

    Added UserEvents and Popover T&C

--- sundae on demand/src/pages/SummaryForm.jsx
+++ sundae on demand/src/pages/SummaryForm.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popoverRight = (
+    <Popover id="popover-positioned-right" title="Popover right">
+      <div>
+        <Popover.Body>No Icecream will actually be delivered.</Popover.Body>
+      </div>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to{" "}
+      <span style={{ color: "blue" }}>
+        <OverlayTrigger placement="right" overlay={popoverRight}>
+          <span>Terms and Conditions</span>
+        </OverlayTrigger>
+      </span>
     </span>
   );
 
--- sundae on demand/src/tests/SummaryForm.test.jsx
+++ sundae on demand/src/tests/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "../pages/SummaryForm.jsx";
 import SummaryForm from "../pages/SummaryForm.jsx";
 
@@ -14,7 +15,7 @@ test("TC01 : Check wether the confirm order button is disabled by default", () =
   expect(confirmOrderButtonElement).toBeDisabled();
 });
 
-test("TC02 : Check wether the confirm order button is enabled after checking terms and condition check mark", () => {
+test("TC02 : Check wether the confirm order button is enabled after checking terms and condition check mark : Using fireEvent", () => {
   render(<SummaryForm />);
   const termsAndConditionCheckboxElement = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -28,4 +29,36 @@ test("TC02 : Check wether the confirm order button is enabled after checking ter
   fireEvent.click(termsAndConditionCheckboxElement);
   expect(termsAndConditionCheckboxElement).not.toBeChecked();
   expect(confirmOrderButtonElement).toBeDisabled();
+});
+
+test("TC03 : Check wether the confirm order button is enabled after checking terms and condition check mark : Using userEvent", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  const termsAndConditionCheckboxElement = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmOrderButtonElement = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  await user.click(termsAndConditionCheckboxElement);
+  expect(termsAndConditionCheckboxElement).toBeChecked();
+  expect(confirmOrderButtonElement).toBeEnabled();
+  await user.click(termsAndConditionCheckboxElement);
+  expect(termsAndConditionCheckboxElement).not.toBeChecked();
+  expect(confirmOrderButtonElement).toBeDisabled();
+});
+
+test("TC04 : popover responds to hover - negative test", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  const nullPopover = screen.queryByText(
+    /no icecream will actually be delivered/i
+  );
+  expect(nullPopover).toBeNull(); //popover is not displayed when the initial page is loaded
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions); //hover over
+  const popover = screen.getByText(/no icecream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  await user.unhover(termsAndConditions); //unhover over
+  expect(popover).not.toBeInTheDocument();
 });
